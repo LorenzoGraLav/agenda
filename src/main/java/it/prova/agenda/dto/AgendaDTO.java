45ac06ac52d68b2241fd2ee92bb8e361355e6a62
Lorenzo <lorenzogra2022@libero.it>
2023-05-22 17:21:53 +0200

    aggiunto repository

--- src/main/java/it/prova/agenda/dto/AgendaDTO.java
+++ src/main/java/it/prova/agenda/dto/AgendaDTO.java
@@ -1,5 +1,105 @@
 package it.prova.agenda.dto;
 
+import java.time.LocalDateTime;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import it.prova.agenda.model.Agenda;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AgendaDTO {
+
+	private Long id;
+
+	@NotBlank(message = "{descrizione.notblank}")
+	private String descrizione;
+
+	private LocalDateTime dataOraInizio;
+
+	private LocalDateTime dataOraFine;
+
+	@NotNull(message = "{utente.notnull}")
+	private UtenteDTO utente;
+
+	public AgendaDTO() {
+
+	}
+
+	public AgendaDTO(Long id, String descrizione, LocalDateTime dataOraInizio, LocalDateTime dataOraFine,
+			UtenteDTO utente) {
+		super();
+		this.id = id;
+		this.descrizione = descrizione;
+		this.dataOraInizio = dataOraInizio;
+		this.dataOraFine = dataOraFine;
+		this.utente = utente;
+	}
+
+	public AgendaDTO(Long id, String descrizione, LocalDateTime dataOraInizio, LocalDateTime dataOraFine) {
+		super();
+		this.id = id;
+		this.descrizione = descrizione;
+		this.dataOraInizio = dataOraInizio;
+		this.dataOraFine = dataOraFine;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public LocalDateTime getDataOraInizio() {
+		return dataOraInizio;
+	}
+
+	public void setDataOraInizio(LocalDateTime dataOraInizio) {
+		this.dataOraInizio = dataOraInizio;
+	}
+
+	public LocalDateTime getDataOraFine() {
+		return dataOraFine;
+	}
+
+	public void setDataOraFine(LocalDateTime dataOraFine) {
+		this.dataOraFine = dataOraFine;
+	}
+
+	public UtenteDTO getUtente() {
+		return utente;
+	}
+
+	public void setUtente(UtenteDTO utente) {
+		this.utente = utente;
+	}
+
+	public Agenda buildAgendaModel(boolean includeUtente) {
+		Agenda result = new Agenda(this.id, this.descrizione, this.dataOraInizio, this.dataOraFine);
+
+		if (includeUtente && utente != null)
+			result.setUtente(this.utente.buildUtenteModel(false));
+		return result;
+	}
+
+	public static AgendaDTO buildAgendaDTOFromModel(Agenda agendaModel) {
+		AgendaDTO result = new AgendaDTO(agendaModel.getId(), agendaModel.getDescrizione(),
+				agendaModel.getDataOraInizio(), agendaModel.getDataOraFine(),
+				UtenteDTO.buildUtenteDTOFromModel(agendaModel.getUtente()));
+
+		return result;
+	}
 
 }
--- src/main/java/it/prova/agenda/model/Agenda.java
+++ src/main/java/it/prova/agenda/model/Agenda.java
@@ -2,6 +2,7 @@ package it.prova.agenda.model;
 
 import java.time.LocalDateTime;
 
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -12,6 +13,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+
 @Entity
 @Table(name = "agenda")
 public class Agenda {
@@ -20,8 +22,8 @@ public class Agenda {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
-	@Column(name = "descrzione")
-	private String descrzione;
+	@Column(name = "descrizione")
+	private String descrizione;
 	@Column(name = "dataorainizio")
 	private LocalDateTime dataOraInizio;
 	@Column(name = "dataorafine")
@@ -35,13 +37,24 @@ public class Agenda {
 		super();
 	}
 
-	public Agenda(Long id, String descrzione, LocalDateTime dataOraInizio, LocalDateTime dataOraFine, Utente utente) {
+	public Agenda(Long id, String descrizione, LocalDateTime dataOraInizio, LocalDateTime dataOraFine, Utente utente) {
 		super();
 		this.id = id;
-		this.descrzione = descrzione;
+		this.descrizione = descrizione;
 		this.dataOraInizio = dataOraInizio;
 		this.dataOraFine = dataOraFine;
 		this.utente = utente;
+	}
+	
+	
+	
+
+	public Agenda(Long id, String descrizione, LocalDateTime dataOraInizio, LocalDateTime dataOraFine) {
+		super();
+		this.id = id;
+		this.descrizione = descrizione;
+		this.dataOraInizio = dataOraInizio;
+		this.dataOraFine = dataOraFine;
 	}
 
 	public Long getId() {
@@ -52,12 +65,12 @@ public class Agenda {
 		this.id = id;
 	}
 
-	public String getDescrzione() {
-		return descrzione;
+	public String getDescrizione() {
+		return descrizione;
 	}
 
-	public void setDescrzione(String descrzione) {
-		this.descrzione = descrzione;
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
 	}
 
 	public LocalDateTime getDataOraInizio() {
@@ -83,5 +96,6 @@ public class Agenda {
 	public void setUtente(Utente utente) {
 		this.utente = utente;
 	}
-
+	
+	
 }
